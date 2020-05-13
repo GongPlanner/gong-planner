c45f0210ea5133669f352003c43af2b2e42ca8b9
SsangWoo <tkddn204@gmail.com>
2020-05-13 23:37:15 +0900

    확대 off, 창 세로 길이 고정

--- src/router/Home.js
+++ src/router/Home.js
@@ -1,38 +1,43 @@
-import React from 'react';
-import _ from 'lodash';
-import { Grid } from '@material-ui/core';
-import { styled } from '@material-ui/styles';
-import TodayDate from '../components/TodayDate';
-import EditSetting from '../components/EditSetting';
-import Todaylog from '../components/Todaylog';
-import Subjects from '../components/Subjects';
+import React from "react";
+import _ from "lodash";
+import { Grid } from "@material-ui/core";
+import { styled } from "@material-ui/styles";
+import TodayDate from "../components/TodayDate";
+import EditSetting from "../components/EditSetting";
+import Todaylog from "../components/Todaylog";
+import Subjects from "../components/Subjects";
 // import Timetable from '../components/Timetable';
-import CvsTimetable from '../components/CvsTimetable';
+import CvsTimetable from "../components/CvsTimetable";
 
 const RootGrid = styled(Grid)({
-  height: '100vh',
-  flexDirection: 'column',
-  flexWrap: 'nowrap',
+  height: "100vh",
+  overflow: "hidden",
+  flexDirection: "column",
+  flexWrap: "nowrap"
 });
 const HeaderGrid = styled(Grid)({
-  display: 'flex',
-  height: '64px',
-  justifyContent: 'space-between',
-  flexWrap: 'nowrap',
+  display: "flex",
+  height: "64px",
+  justifyContent: "space-between",
+  flexWrap: "nowrap"
 });
 const LogGrid = styled(Grid)({});
 const BottomGrid = styled(Grid)({
-  height: '100%',
-  '& div': {
-    height: '100%',
-  },
+  overflow: "scroll",
+  height: "100%",
+  "& div": {
+    height: "100%"
+  }
+});
+const SubjectGrid = styled(Grid)({
+  overflow: "scroll"
 });
 
-const Home = (props) => {
+const Home = props => {
   const { onChangeTodayData, onChangeSubjects, onChangeTimeTable } = props;
   const todayData = props.todayData || {};
   const collectionName = _.isEmpty(props.match.params)
-    ? 'testSubject'
+    ? "testSubject"
     : props.match.params.colName;
 
   return (
@@ -42,21 +47,21 @@ const Home = (props) => {
         <EditSetting colName={collectionName} />
       </HeaderGrid>
       <LogGrid item>
-        <Todaylog 
+        <Todaylog
           colName={collectionName}
           date={todayData.date}
           todayData={todayData}
         />
       </LogGrid>
       <BottomGrid item container spacing={1}>
-        <Grid item xs={6}>
-          <Subjects 
+        <SubjectGrid item xs={6}>
+          <Subjects
             colName={collectionName}
             date={todayData.date}
             onChangeSubjects={onChangeSubjects}
-            subjects={todayData.subjects} 
+            subjects={todayData.subjects}
           />
-        </Grid>
+        </SubjectGrid>
         <Grid item xs={6}>
           {/* <Timetable today={todayData.date} timeTable={todayData.timeTable ? todayData.timeTable : []} /> */}
           <CvsTimetable
