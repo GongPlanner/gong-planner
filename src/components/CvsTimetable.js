7732961652c5a8bceafe299550bd79e88b060f57
SsangWoo <tkddn204@gmail.com>
2020-05-14 10:55:41 +0900

    [HotFix] 타임테이블 추가 시 endtime+1시간 안되게 변경

--- src/components/CvsTimetable.js
+++ src/components/CvsTimetable.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
-import dayjs from 'dayjs';
-import { styled } from '@material-ui/styles';
-import { Stage, Layer, Rect, Line, Text } from 'react-konva';
-import TimetableModal from '../components/Modals/TimetableModal';
-import TimeTable from '../lib/timetable';
+import React, { Component } from "react";
+import _ from "lodash";
+import dayjs from "dayjs";
+import { styled } from "@material-ui/styles";
+import { Stage, Layer, Rect, Line, Text } from "react-konva";
+import TimetableModal from "../components/Modals/TimetableModal";
+import TimeTable from "../lib/timetable";
 
 const StyledStage = styled(Stage)({
-  height: '100%',
+  height: "100%"
 });
 
 export default class CvsTimetable extends Component {
@@ -15,48 +15,48 @@ export default class CvsTimetable extends Component {
     width: 240,
     height: 550,
     showModal: false,
-    modalRole: 'add',
+    modalRole: "add",
     modalSubject: _.isEmpty(this.props.subjects)
       ? null
       : this.props.subjects[_.keys(this.props.subjects)[0]].subjectName,
     modalStartDayjs: dayjs(),
     modalEndDayjs: dayjs(),
-    timeTable: new TimeTable(this.props.timeTable),
+    timeTable: new TimeTable(this.props.timeTable)
   };
 
   componentDidMount() {
     this.checkSize();
-    window.addEventListener('resize', this.checkSize.bind(this));
+    window.addEventListener("resize", this.checkSize.bind(this));
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.checkSize.bind(this));
+    window.removeEventListener("resize", this.checkSize.bind(this));
   }
 
   handleModal(time) {
     if (time.startDayjs) {
       // 편집 창
-      this.setState((prevState) => {
+      this.setState(prevState => {
         return {
           ...prevState,
-          modalRole: 'edit',
+          modalRole: "edit",
           modalSubject: time.subject,
           modalStartDayjs: time.startDayjs,
           modalEndDayjs: time.endDayjs,
-          showModal: !prevState.showModal,
+          showModal: !prevState.showModal
         };
       });
     } else {
-      this.setState((prevState) => {
+      this.setState(prevState => {
         return {
           ...prevState,
-          modalRole: 'add',
+          modalRole: "add",
           modalSubject: _.isEmpty(this.props.subjects)
             ? null
             : this.props.subjects[_.keys(this.props.subjects)[0]].subjectName,
           modalStartDayjs: dayjs(),
-          modalEndDayjs: dayjs().add(1, 'hour'),
-          showModal: _.isBoolean(time) ? time : !prevState.showModal,
+          modalEndDayjs: dayjs(),
+          showModal: _.isBoolean(time) ? time : !prevState.showModal
         };
       });
     }
@@ -84,7 +84,7 @@ export default class CvsTimetable extends Component {
       modalSubject,
       modalStartDayjs,
       modalEndDayjs,
-      showModal,
+      showModal
     } = this.state;
 
     const hourWidth = 30;
@@ -94,14 +94,14 @@ export default class CvsTimetable extends Component {
     return (
       <React.Fragment>
         <StyledStage
-          ref={(element) => (this.element = element)}
+          ref={element => (this.element = element)}
           width={maxWidth}
           height={maxHeight}
         >
-          <Layer key={0} name='frame'>
+          <Layer key={0} name="frame">
             <Line
               points={[hourWidth, 0, hourWidth, maxHeight]}
-              stroke='black'
+              stroke="black"
               strokeWidth={1}
             />
             {[...Array(24)].map((_, i) => {
@@ -118,13 +118,13 @@ export default class CvsTimetable extends Component {
                     height={cellHeight}
                     maxHeight={cellHeight}
                     text={`${i}시`}
-                    align='center'
-                    verticalAlign='middle'
+                    align="center"
+                    verticalAlign="middle"
                     fontSize={11}
                   />
                   <Line
                     points={[0, y, maxWidth, y]}
-                    stroke='black'
+                    stroke="black"
                     strokeWidth={1}
                   />
                 </React.Fragment>
@@ -136,13 +136,13 @@ export default class CvsTimetable extends Component {
                 <Line
                   key={x}
                   points={[x, 0, x, maxHeight]}
-                  stroke='black'
+                  stroke="black"
                   strokeWidth={0.5}
                 />
               );
             })}
           </Layer>
-          <Layer key={1} name='click-frame'>
+          <Layer key={1} name="click-frame">
             <Rect
               x={hourWidth}
               y={0}
@@ -152,20 +152,20 @@ export default class CvsTimetable extends Component {
               onTap={this.handleModal.bind(this)}
             />
           </Layer>
-          <Layer key={2} name='study'>
+          <Layer key={2} name="study">
             {_.isEmpty(timeTable) ? (
               <React.Fragment />
             ) : (
               timeTable.studyTime.map((arr, hour) => {
                 if (arr) {
                   const y = hour * cellHeight;
-                  return arr.map((study) => {
+                  return arr.map(study => {
                     const x = hourWidth + study.start * (restWidth / 60);
                     const width = (study.end - study.start) * (restWidth / 60);
                     const time = {
                       subject: study.subject,
                       startDayjs: study.startDayjs,
-                      endDayjs: study.endDayjs,
+                      endDayjs: study.endDayjs
                     };
                     return (
                       <Rect
